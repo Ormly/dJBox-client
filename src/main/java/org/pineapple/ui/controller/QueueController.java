75e0e4188036a38e3bef8ea9d8dfdb35ce9769c9
Tim <jeditim@hotmail.co.uk>
2019-12-05 17:57:54 +0100

    Timer clears current view and replaces with latest queue if it passes the response state

--- src/main/java/org/pineapple/ui/controller/QueueController.java
+++ src/main/java/org/pineapple/ui/controller/QueueController.java
@@ -1,11 +1,18 @@
 package org.pineapple.ui.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.pineapple.Main;
 import org.pineapple.core.JukeBoxClient;
+import org.pineapple.core.ResponseState;
+import org.pineapple.core.Song;
 import org.pineapple.ui.scene.Scene;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QueueController extends Controller {
 
@@ -27,5 +34,23 @@ public class QueueController extends Controller {
         dialog.setTitle("dJBox - Library");
         dialog.getIcons().add(new Image("ananas_color.png"));
         dialog.show();
+    }
+
+    public List<Song> doGetQueue() {
+        ResponseState responseState = jukeBoxClient.getQueueResponseState();
+        List<Song> songList = new ArrayList<>();
+        switch(responseState) {
+            case SUCCESS:
+                System.out.println("doGetQueue SUCCESS");
+                songList = jukeBoxClient.doGetQueue();
+                break;
+            case AUTHFAIL:
+                System.out.println("doGetQueue AUTHFAIL");
+                break;
+            case FATAL:
+                System.out.println("doGetQueue FATAL");
+                break;
+        }
+        return songList;
     }
 }
